e7c45122379b4a07a3cf7576af9ecf65cd62375b
Aleksey Kramer <xxxxx@gmail.com>
2017-02-10 16:29:26 -0800

    completed stup.py

--- sparser/setup.py
+++ sparser/setup.py
@@ -2,5 +2,20 @@ from setuptools import setup
 
 setup (
     name="sparser",
-    version="0.1"
+    version="0.1",
+    author="Aleksey Kramer",
+    description='news web scraper',
+    url='https://github.com/voite1/sparser',
+    
+    install_requires = [
+        'joblib', 
+        'beautifulsoup4'
+        ],
+       
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        ],
     )
+
