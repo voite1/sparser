b14e030c12db9e35824b5eb82229fa84f45f7124
Aleksey Kramer <xxxxx@gmail.com>
2017-02-10 16:40:17 -0800

    completed setup.py

--- sparser/setup.py
+++ sparser/setup.py
@@ -1,17 +1,28 @@
 from setuptools import setup
 
 setup (
+    # Project name
     name="sparser",
+    
+    # Project version
     version="0.1",
+    
+    # Project author
     author="Aleksey Kramer",
+    
+    # Project description
     description='news web scraper',
+    
+    # Project URL
     url='https://github.com/voite1/sparser',
     
+    # Required prerequisites 
     install_requires = [
         'joblib', 
         'beautifulsoup4'
         ],
-       
+    
+    # Project classifiers
     classifiers=[
         'Programming Language :: Python :: 3.6',
         ],
