2262ce5ad3d1e574f142b46c0d96946e78e78938
Aleksey Kramer <xxxxx@gmail.com>
2017-02-10 16:30:38 -0800

    cleanup

--- sparser/setup.py
+++ sparser/setup.py
@@ -13,8 +13,6 @@ setup (
         ],
        
     classifiers=[
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         ],
     )
