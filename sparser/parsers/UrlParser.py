6e53db1695495640b78948eb8c9330f766d4033e
Aleksey Kramer <xxxxx@gmail.com>
2017-02-15 12:25:42 -0800

    code updates

--- sparser/parsers/UrlParser.py
+++ sparser/parsers/UrlParser.py
@@ -1,4 +1,17 @@
+from joblib import Parallel, delayed
+from sparser.workers.worker import processGoogleURL
 
+global URLS
 
-def GetFileURLs(url):
-    pass
+def scrapeURL(url): 
+    a = Parallel(n_jobs=3)(delayed(processGoogleURL)(url) for i in range(1))
+    return a
+
+def scrapeURLs(url_list):
+    a = Parallel(n_jobs=3)(delayed(processGoogleURL)(i) for i in url_list)
+
+if __name__ == "__main__":
+    lst = scrapeURL('http://news.google.com')
+    lst = scrapeURLs(lst[0])
+    for i in lst: 
+        print(len(i))
